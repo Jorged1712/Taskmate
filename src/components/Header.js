c23a460247a6dafef599c233d63bb1cbeacba745
jorged1712 <jotadaniel93@gmail.com>
2024-06-05 15:48:39 -0400

    final

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ export const Header = () => {
 
       <div className="themeSelector">
         <span onClick={() => setTheme("light")} className={theme==="light" ? "light activeTheme" : "light "}></span>
-        <span onClick={() => setTheme("medium")} className={theme==="medium" ? "medium ctiveTheme" : "medium"}></span>
+        <span onClick={() => setTheme("medium")} className={theme==="medium" ? "medium activeTheme" : "medium"}></span>
         <span onClick={() => setTheme("dark")} className={theme==="dark" ? "dark activeTheme" : "dark"}></span>
         <span onClick={() => setTheme("gOne") }  className={theme==="gOne" ? "gOne activeTheme" : "gOne"}></span>
         <span onClick={() => setTheme("gTwo") } className={theme==="gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
